5e74993a621918b2977c6db36c5eb93f447d69f3
Lasse Lybeck <lybeck.lasse@gmail.com>
2016-01-30 12:38:03 +0200

    Added scripts and triggers for generating new terrains on the fly.

---
+++ Assets/Scripts/TerrainGenerationTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainGenerationTrigger : MonoBehaviour {
+
+	public TerrainManager terrainManager;
+
+	void Start() {
+		terrainManager = GameObject.Find("TerrainManager").GetComponent<TerrainManager>();
+	}
+
+	void OnTriggerExit(Collider collider) {
+		Debug.Log("Name: " + collider.gameObject.name);
+		terrainManager.nextTerrain();
+		GetComponent<BoxCollider>().enabled = false;
+	}
+}
