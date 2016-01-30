15c9b013231b097ec9ff1485410492688ae88f1a
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2016-01-30 16:37:39 +0200

    #8 - Preparing for a non-playable dog.

---
+++ Assets/CameraFollowScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowScript : MonoBehaviour {
+
+    public Vector3 offset = new Vector3(1.25f, 1.5f, -2.5f);
+    public Vector3 eulerAngles = new Vector3(25f, 0f, 0f);
+    	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.transform.position = transform.position + offset;
+        Camera.main.transform.eulerAngles = eulerAngles;
+	}
+}
