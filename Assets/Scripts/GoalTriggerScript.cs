0bc260bfd7f130aa3d3ea7048481105108b3f6e6
Lasse Lybeck <lybeck.lasse@gmail.com>
2016-01-31 15:07:58 +0200

    Added goal trigger script.

--- Assets/Scripts/GoalTriggerScript.cs
+++ Assets/Scripts/GoalTriggerScript.cs
@@ -3,7 +3,28 @@ using System.Collections;
 
 public class GoalTriggerScript : MonoBehaviour {
 
+	private bool done = false;
+	private bool timerOn = false;
+	public float waitingTime = 2.0f;
+	private float timer = 0.0f;
+
 	void OnTriggerExit(Collider collider) {
 		Debug.Log("Goal!");
+		done = true;
+		timerOn = false;
+	}
+
+	public void Update() {
+		if (done) {
+			if (timerOn) {
+				timer += Time.deltaTime;
+			} else {
+				timerOn = true;
+				timer = 0.0f;
+			}
+			if (timer >= waitingTime) {
+				Application.LoadLevel("EndScene");
+			}
+		}
 	}
 }
