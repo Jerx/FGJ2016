034aad6711adf4c80a6a910e559167aad47e951b
Christoffer Fridlund <christoffer.fridlund@hotmail.com>
2016-01-31 13:11:51 +0200

    Starting game on any button press, quitting on escape.

--- Assets/MenuButtonScript.cs
+++ Assets/MenuButtonScript.cs
@@ -2,14 +2,16 @@
 using System.Collections;
 
 public class MenuButtonScript : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("Escape Key pressed");
+            Application.Quit();
+        } else if (Input.anyKeyDown) {
+            Debug.Log("An other key is down");
+            Application.LoadLevel("GameScene");
+        }
 	}
+
 }
